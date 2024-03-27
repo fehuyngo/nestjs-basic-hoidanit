a5f762f97aafd0fca3522e13ea5a33b98c2c5eea
fehuyngo <fe.huyngo@gmail.com>
2024-03-27 09:00:37 +0700

    3

--- nestjs-basic/src/app.controller.ts
+++ nestjs-basic/src/app.controller.ts
@@ -7,7 +7,11 @@ export class AppController {
 
   @Get()
   @Render("home")
-  getHello() {
+  handleHomePage() {
+    const message1 = this.appService.getHello();
+    return {
+      message: message1
+    }
     // return "this.appService.getHello()";
   }
 }
